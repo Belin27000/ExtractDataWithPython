0b397bfdc7f40ebdf42fce163f831c3a67469b8e
Belin27000 <y.lecerf@protonmail.com>
2024-03-01 10:43:44 +0100

    clean mail adresses script

--- cleanMail.py
+++ cleanMail.py
@@ -1,7 +1,11 @@
 
 #Lire les adresses mail dans un fichier texte
 with open('adresse_email_To_Clean.txt',"r") as file:
-    emails = file.read()
+    emails = file.readlines()
     print(emails)
 # Retirer les doublons en convertissant la liste en un ensemble (set)
-    
+unique_emails = set(emails)
+with open("adresses_email_uniques.txt","w") as file:
+    for email in unique_emails:
+        file.write(email)
+print("Les adresses e-mail uniques ont été enregistrées dans adresses_email_uniques.txt")
